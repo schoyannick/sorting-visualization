1ba2d8f4812a858716e3605f0a7f25c2624b5672
Yannick Scho <yannick.scho@tobit.com>
2021-08-13 21:08:37 +0200

    :sparkles: Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import React from 'react';
 
 import App from '../src/components/App';
 import { handleGetStaticProps } from '../src/utils/handleGetStaticProps';
 
 const Index: React.FC = () => (
-    <App/>
+    <>
+        <Head>
+            <title>Sorting Visualization</title>
+        </Head>
+        <App/>
+    </>
 );
 
 export const getStaticProps: GetStaticProps = (context) => handleGetStaticProps();
